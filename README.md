1060dfc2c6ca8fbf90d05cab164abb44007131d6
Phses <phsouzaesilva@gmail.com>
2022-04-25 20:18:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Frontend Mentor - Advice generator app solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+  - [Useful resources](#useful-resources)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Gerar um conselho aleatório;
+- Ver a quantidade de conselhos gerados;
+
+### Screenshot
+
+![](./images/Captura-de-tela.JPG)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- JS Vanilla
+- API
+
+### What I learned
+
+Consumir uma API realizando requisições AJAX
+
+```js
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.adviceslip.com/advice", true);
+let output = "";
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    const response = JSON.parse(xhr.responseText);
+    output = `"${response.slip.advice}"`;
+    quote.textContent = output;
+    saveQuoteLocalStorage(output);
+  } else {
+    alert("error");
+  }
+};
+xhr.send();
+```
+
+### Useful resources
+
+- [https://api.adviceslip.com/] - Advice Slip JSON API
---
+++ app.js
@@ -0,0 +1,67 @@
+const quote = document.querySelector(".card__quote");
+const counterMessage = document.querySelector(".counter");
+const cardDivider = document.querySelector(".card__divider");
+let counter = 0;
+
+function saveCounterLocalStorage(lastCounter) {
+  localStorage.setItem("counter", JSON.stringify(lastCounter));
+}
+
+function saveQuoteLocalStorage(quote) {
+  localStorage.setItem("quote", quote);
+}
+
+function changeImages() {
+  if (window.innerWidth <= 500) {
+    cardDivider
+      .querySelector("img")
+      .setAttribute("src", "./images/pattern-divider-mobile.svg");
+  } else if (window.innerWidth > 500) {
+    cardDivider
+      .querySelector("img")
+      .setAttribute("src", "./images/pattern-divider-desktop.svg");
+  }
+  console.log(cardDivider.querySelector("img"));
+}
+
+document.querySelector(".card__btn").addEventListener("click", function (e) {
+  counter++;
+  saveCounterLocalStorage(counter);
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api.adviceslip.com/advice", true);
+  let output = "";
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      output = `"${response.slip.advice}"`;
+      quote.textContent = output;
+      saveQuoteLocalStorage(output);
+    } else {
+      alert("error");
+    }
+  };
+  xhr.send();
+
+  counterMessage.textContent = counter.toString();
+  e.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("counter") === "null") {
+    counter = 0;
+  } else {
+    counter = parseInt(JSON.parse(localStorage.getItem("counter")));
+  }
+  counterMessage.textContent = counter.toString();
+  let output = "";
+  if (localStorage.getItem("quote") === null) {
+    output = "";
+  } else {
+    output = localStorage.getItem("quote");
+  }
+  quote.textContent = output;
+  changeImages();
+});
+
+window.addEventListener("resize", changeImages);
